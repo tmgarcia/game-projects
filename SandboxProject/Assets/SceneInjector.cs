beff4a3e54deee8eafb84de7b5d3485f8a977a24
Jonathan Pearl <jpearl@student.neumont.edu>
2015-05-02 21:12:51 -0600

    Moves demo in sandbox and adds some networky things

---
+++ SandboxProject/Assets/SceneInjector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SceneInjector : MonoBehaviour {
+
+    public NetworkLobbyManager manager;
+
+    public string onlineScene = "Game";
+    public string offlineScene = "Lobby";
+
+	// Use this for initialization
+	void Start () {
+        manager.playScene = onlineScene;
+        manager.lobbyScene = offlineScene;
+	}
+}
